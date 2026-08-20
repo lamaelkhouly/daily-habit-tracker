60054e359c762a48be65a26a0b4a353d3d36d181
lamaelkhouly <lamaelkhoulyy@gmail.com>
2026-08-20 16:56:20 +0300

    Add JUnit tests, Mockito tests and Postman collection

--- src/main/java/org/internship/dailyhabittracker/domain/depricated/Role.java
+++
@@ -1,5 +0,0 @@
-package org.internship.dailyhabittracker.domain.depricated;
-
-public enum Role {
-    ADMIN,USER
-}
--- src/main/java/org/internship/dailyhabittracker/service/HabitService.java
+++
@@ -1,8 +0,0 @@
-package org.internship.dailyhabittracker.service;
-
-import jakarta.persistence.Entity;
-import org.springframework.stereotype.Service;
-
-@Service
-public class HabitService {
-}
